a1b31aee58a9db2967f07ce38d72ecdc41a3ed66
xSuui <samuel2012rogerio@gmail.com>
2024-02-07 15:09:25 -0300

    add double jump and animations

--- Plataformer - Metroidvania/Assets/Scripts/Player/Player.cs
+++ Plataformer - Metroidvania/Assets/Scripts/Player/Player.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rig;
-    public float speed;
+    public Animator anim;
 
+    public float speed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool doubleJump;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
     }
 
     void FixedUpdate()
@@ -33,11 +38,53 @@ public class Player : MonoBehaviour
 
         if(movement > 0)
         {
+            if(!isJumping)
+            {
+                anim.SetInteger("Transition", 1);
+            }
+
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        if(movement > 0)
+        if(movement < 0)
         {
+            if (!isJumping)
+            {
+                anim.SetInteger("Transition", 1);
+            }
+
             transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if(movement == 0 && !isJumping)
+        {
+            anim.SetInteger("Transition", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+            {
+                anim.SetInteger("Transition", 2);
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isJumping = true;
+                doubleJump = true;
+            }
+            else if(doubleJump)
+            {
+                anim.SetInteger("Transition", 2);
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D colisor)
+    {
+        if(colisor.gameObject.layer == 6)
+        {
+            isJumping = false;
         }
     }
 }
